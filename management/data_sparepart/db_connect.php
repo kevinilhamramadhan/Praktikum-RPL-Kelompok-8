aadbc3400aaabb1fc1fdcafbfa8e71117242c798
Nevin Aldora <kayananevin18@gmail.com>
2025-05-13 19:38:29 +0700

    db_connect.php

--- management/data_sparepart/db_connect.php
+++ management/data_sparepart/db_connect.php
@@ -1,12 +1,11 @@
 <?php
 $host = "localhost";
 $user = "root";
-$password = "";  // kosongkan jika memang tidak ada password
+$password = "";  
 $dbname = "inventory_db";
 
 $conn = new mysqli($host, $user, $password, $dbname);
 
-// Cek koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
