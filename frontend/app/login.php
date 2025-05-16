fc1c1bff44962935a11cd19b86943f439f02a04c
okfn05 <164615332+okfn05@users.noreply.github.com>
2025-05-16 20:35:22 +0700

    Update login

--- frontend/app/login.php
+++ frontend/app/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ReAngel Login</title>
+    <title>ReFairy Login</title>
     <link rel="stylesheet" href="../style/login_style.css">
 </head>
 
