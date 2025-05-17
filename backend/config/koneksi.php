d8b5aa9170f97e891076782490d65f3be309dc65
okfn05 <164615332+okfn05@users.noreply.github.com>
2025-05-17 10:02:17 +0700

    update

--- backend/config/koneksi.php
+++ backend/config/koneksi.php
@@ -1,13 +1,13 @@
 <?php
-$host = "localhost";      // atau "127.0.0.1"
-$user = "root";           // user default XAMPP
-$pass = "";               // kosong jika pakai XAMPP
+$host = "localhost";      
+$user = "root";           
+$pass = "";               
 $db   = "Refairy";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$koneksi = mysqli_connect($host, $user, $pass, $db);
 
 // Cek koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+if (!$koneksi) {
+    die("Koneksi gagal: ");
 }
 ?>
